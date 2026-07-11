fc3b32294e7307689061964210a34238ce5313e9
karmakarila5-star <karmakarila5@gmail.com>
2026-07-11 19:30:55 +0530

    while loop homework

--- reverseorder.py
+++ reverseorder.py
@@ -1,4 +1,16 @@
-n = int(input("enter the value of n: "))
-print ("number from {0} to {1} are: ".format(n,1))
-for i in range(n,0,-1):
-    print(i)
+num1 = int(input("Enter the first number: "))
+n = int(input("Enter the second number: "))
+
+answer = 1
+count = 0
+
+while count < n:
+    answer = answer * num1
+    count = count + 1
+
+print("The first number was:")
+print(num1)
+print("The power n was:")
+print(n)
+print("The final calculation result is:")
+print(answer)
